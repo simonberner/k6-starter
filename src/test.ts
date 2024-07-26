5d83eb9cba17d9c291371cc07d64a322def3a75d
Simon Berner <sibebzh@gmail.com>
2024-07-26 16:06:37 +0200

    test: :white_check_mark: add a basic test

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+import {sleep, check} from 'k6';
+import http from 'k6/http';
+
+export let options = {
+    vus: 10,
+    duration: '30s',
+};
+
+export default function () {
+    const res = http.get('https://test-api.k6.io/public/crocodiles/1/');
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+    sleep(1);
+}
