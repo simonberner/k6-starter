63541633d7c28b25599f6ab3e4f6b111868f6b9b
Simon Berner <sibebzh@gmail.com>
2024-08-01 13:39:04 +0200

    refactor: :recycle: move pom to the test
    Move LoginPage pom to the test because otherwise the import of the class does not work yet straight forward.

--- src/browser-test.ts
+++ src/browser-test.ts
@@ -1,6 +1,6 @@
-import {browser} from 'k6/browser'
+import {browser, Locator, Page} from 'k6/browser'
 import {check} from "k6";
-import {LoginPage} from "./page_objects/login-page";
+// import {LoginPage} from "./page_objects/login-page.ts";
 
 export const options = {
     scenarios: {
@@ -37,5 +37,38 @@ export default async function browserTest() {
 
     } finally {
         await page.close();
+    }
+}
+
+/*
+Page Object Model is a well-known pattern to abstract a web page.
+
+The Locator API enables using the Page Object Model pattern to organize
+and simplify test code.
+ */
+class LoginPage {
+
+    private page: Page;
+    private loginTextField: Locator;
+    private passwordField: Locator;
+    private loginButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.loginTextField = page.locator('input[name="login"]');
+        this.passwordField = page.locator('input[name="password"]');
+        this.loginButton = page.locator('input[type="submit"]');
+    }
+
+    public async login(username: string, password: string): Promise<void> {
+        await Promise.all([
+            this.loginTextField.fill(username),
+            this.passwordField.fill(password),
+            this.loginButton.click()
+        ])
+    }
+
+    public async gotoLoginPage() {
+        return await this.page.goto("https://test.k6.io/my_messages.php");
     }
 }
